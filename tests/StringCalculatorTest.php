d66fc6c57e86820d9100b82b80cb1599de9bb0b5
frez_freedom <frez1374@gmail.com>
2023-09-02 11:40:50 +0330

    Add more passed tests

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -7,14 +7,88 @@ require_once(__DIR__. '/../ParserMain.php');
 
 final class StringCalculatorTest extends TestCase
 {
-    public function testAddFunction()
+    /**
+     * @dataProvider provideStringCalculatorClassData
+     */
+    public function testAddFunction($expected_ans, $ans): void
+    {
+        $this->assertEquals($expected_ans, $ans);
+    }
+
+    public static function provideStringCalculatorClassData()
     {
         $tokenizer = new Tokenizer();
         $parser = new ParserMain($tokenizer);
-
         $stringCalculator = new StringCalculator($parser);
+
+        // ---------------------------------------------------------
         $ans = $stringCalculator->add('1,2');
 
-        $this->assertEquals(3, $ans);
+        yield [
+            3,
+            $ans,
+        ];
+
+        // ---------------------------------------------------------
+        $ans = $stringCalculator->add('1,4');
+
+        yield [
+            5,
+            $ans,
+        ];
+
+        // ---------------------------------------------------------
+        $ans = $stringCalculator->add('10');
+
+        yield [
+            10,
+            $ans,
+        ];
+
+        // ---------------------------------------------------------
+        $ans = $stringCalculator->add('');
+
+        yield [
+            0,
+            $ans,
+        ];
+
+        // ---------------------------------------------------------
+        $ans = $stringCalculator->add('10,4,20,30');
+
+        yield [
+            64,
+            $ans,
+        ];
+
+        // ---------------------------------------------------------
+        $ans = $stringCalculator->add('1,2\n3');
+
+        yield [
+            6,
+            $ans,
+        ];
+
+        // ---------------------------------------------------------
+        $ans = $stringCalculator->add('1,2\n3');
+
+        yield [
+            6,
+            $ans,
+        ];
     }
+
+
+//    public function testCustomParserExceptionOfAddFunction(): void
+//    {
+//        $this->expectException(CustomParserException::class);
+//        $this->expectExceptionMessage('Consecutive delimiter tokens');
+//
+//        $tokenizer = new Tokenizer();
+//        $parser = new ParserMain($tokenizer);
+//        $stringCalculator = new StringCalculator($parser);
+//
+//        $ans = $stringCalculator->add('1,2');
+//    }
+
 }
